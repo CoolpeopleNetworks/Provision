0b8b0088907fe84e600abdb8bd34d45f4bfe37ce
John W. Terrell <john@coolpeoplenetworks.com>
2023-09-27 16:06:40 -0700

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 # NOTE: Use this script to bootstrap a system which will install required packages for ansible-pull
-sudo apt install -y openssh-server git ansible
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+fi
+
+if [ "$NAME" == "Ubuntu" || "$NAME" == "Debian" ]; then
+    sudo apt install -y openssh-server git ansible
+fi
