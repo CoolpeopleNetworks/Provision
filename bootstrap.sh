c1e626f23b3320c82fe7b03f21b12976145aebb8
John W. Terrell <john@coolpeoplenetworks.com>
2023-10-16 17:39:39 -0700

    Added headscale node

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ fi
 
 case "$NAME" in 
     "Ubuntu" | "Debian")
-        sudo apt install -y snapd git ansible
+        sudo apt install -y snapd cron git ansible
         ;;
     *)
         echo "OS Name $NAME not recognized"
